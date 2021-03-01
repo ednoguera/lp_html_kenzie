de87b7d7d626528f822230ead52458a893816861
Edson Silva <edsonoguera@outlook.com>
2021-03-01 16:22:41 -0300

    feat: deleted main.js

--- src/main.js
+++
@@ -1,9 +0,0 @@
-const url = "https://startsuacarreira.kenzie.com.br/?utm_source=teste&utm_medium=utmbuilder&utm_campaign=daniel"
-
-const newUrl = url.split("?")
-const urlParams = newUrl[1]
-const params = urlParams.split("&")
-
-console.log(newUrl)
-console.log(urlParams)
-console.log(params[1].split("=")[1])
