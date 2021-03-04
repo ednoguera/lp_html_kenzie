41da005dbb615f40aa4071383b6104513a4c23b9
Mariana Antunes <marimsantunes@gmail.com>
2021-03-04 13:40:29 -0300

    add active campaign form event

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,28 +2,25 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "../pages/home/index";
 import ThankYouPage from "../pages/typ";
 
+const ActiveCampaign = () => {
+  return window.dataLayer.push({ event: "active-campaign-form-sent" });
+  setTimeout(
+    () => (window.location = "https://curso-html-css.joinzap.app/"),
+    1000
+  );
+};
+
 const Router = () => {
-    return (
-        <>
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route
-                    exact
-                    path="/obrigado"
-                    render={() => {
-                        setTimeout(
-                            () =>
-                                (window.location =
-                                    "https://curso-html-css.joinzap.app/"),
-                            1000
-                        );
-                    }}
-                />
-            </Switch>
-        </>
-    );
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/obrigado" render={<ActiveCampaign />} />
+      </Switch>
+    </>
+  );
 };
 
 export default Router;
