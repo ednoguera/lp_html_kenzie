e0a53b5d5ee96f4e720dd53106a48ac8a1fcdf53
Mariana Antunes <marimsantunes@gmail.com>
2021-03-04 13:59:33 -0300

    fix redirect

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,6 @@
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "../pages/home/index";
 import ThankYouPage from "../pages/typ";
-import { useEffect } from "react";
-
-const ActiveCampaign = () => {
-  useEffect(() => {
-    window.dataLayer.push({ event: "active-campaign-form-sent" });
-    setTimeout(
-      () => (window.location = "https://curso-html-css.joinzap.app/"),
-      1000
-    );
-  }, []);
-
-  return <p></p>;
-};
 
 const Router = () => {
   return (
@@ -22,7 +9,16 @@ const Router = () => {
         <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path="/obrigado" render={<ActiveCampaign />} />
+        <Route
+          exact
+          path="/obrigado"
+          render={() => {
+            setTimeout(
+              () => (window.location = "https://curso-html-css.joinzap.app/"),
+              1000
+            );
+          }}
+        />
       </Switch>
     </>
   );
